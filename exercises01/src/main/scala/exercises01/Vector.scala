59da6bf7dbca3501a882132e3a22bee3d46987d8
Andrey Shchegolev <andrey@Air-Andrey.at.urfu.ru>
2023-02-16 12:22:48 +0500

    Refactor

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -3,19 +3,19 @@ package exercises01
 import scala.math.sqrt
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = new Vector(other.x+this.x, other.y+this.y)
+  def +(other: Vector): Vector = new Vector(other.x + this.x, other.y + this.y)
   //
-  def -(other: Vector): Vector = new Vector(this.x-other.x, this.y-other.y)
+  def -(other: Vector): Vector = new Vector(this.x - other.x, this.y - other.y)
 
   def *(scalar: Double): Vector = new Vector(this.x * scalar, this.y * scalar)
 
-  def unary_- : Vector = new Vector(-this.x,-this.y)
+  def unary_- : Vector = new Vector(-this.x, -this.y)
 
   def euclideanLength: Double = sqrt(this.x * this.x + this.y * this.y)
 
   def normalized: Vector = {
     val length = euclideanLength
-    if (length == 0) new Vector(0,0) else new Vector(this.x / length , this.y / length)
+    if (length == 0) new Vector(0, 0) else new Vector(this.x / length, this.y / length)
   }
 
   override def equals(other: Any): Boolean = {
@@ -31,7 +31,7 @@ class Vector(val x: Double, val y: Double) {
 object Vector {
   def fromAngle(angle: Double, length: Double): Vector = new Vector(length * math.cos(angle), length * math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0,0)) ((A,B) => new Vector(A.x +B.x, A.y + B.y))
+  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0, 0))((A, B) => new Vector(A.x + B.x, A.y + B.y))
 
-  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x,arg.y)
+  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }
