ccc0c42ab9425bf62e7bebd3c13281e58ecdd44f
Goodpurpp <shegolevai14@gmai.com>
2023-03-29 23:03:52 +0500

    ready

--- exercises05/src/main/scala/exercises05/parser/Examples.scala
+++ exercises05/src/main/scala/exercises05/parser/Examples.scala
@@ -3,17 +3,32 @@ package exercises05.parser
 import exercises05.either.EitherCombinators._
 import Error._
 
-object Examples {
+import scala.util.matching.Regex
 
-  /**
-    * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть None
-    * если rawUser.passport == None или rawUser.thirdName == None, то и в результирующем User эти поля == None
-    * passport должен быть передан в формате 1234 567890, если не так, то функция должна вернуть None
-    * если rawUser.id не парсится в Long то функция должна вернуть None
-    * если rawUser.banned, то вернуть None
-    * используйте for-comprehension
-    */
-  def transformToOption(rawUser: RawUser): Option[User] = ???
+object Examples {
+  private val passportRegex = new Regex("(\\d{4}) (\\d{6})")
+  def transformToOption(rawUser: RawUser): Option[User] =
+    for {
+      firstName  <- rawUser.firstName
+      secondName <- rawUser.secondName
+      passport   <- transformToOptionPassport(rawUser.passport)
+      id         <- rawUser.id.toLongOption
+      if !rawUser.banned
+    } yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
+
+  private def transformToOptionPassport(passport: Option[String]): Option[Option[Passport]] =
+    passport match {
+      case None => Some(None)
+      case Some(x) =>
+        passportRegex.findFirstMatchIn(x) match {
+          case None => None
+          case Some(y) =>
+            (y.group(1).toLongOption, y.group(2).toLongOption) match {
+              case (_, None) | (None, _)        => None
+              case (Some(series), Some(number)) => Some(Some(Passport(series, number)))
+            }
+        }
+    }
 
   /**
     * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
@@ -29,5 +44,13 @@ object Examples {
     * используйте for-comprehension
     * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
     */
-  def transformToEither(rawUser: RawUser): Either[Error, User] = ???
+  def transformToEither(rawUser: RawUser): Either[Error, User] =
+    for {
+      _          <- if (rawUser.banned) Left(Banned) else Right(true)
+      id         <- Either.fromOption(rawUser.id.toLongOption)(InvalidId)
+      firstName  <- Either.fromOption(rawUser.firstName)(InvalidName)
+      secondName <- Either.fromOption(rawUser.secondName)(InvalidName)
+      passport   <- Either.fromOption(transformToOptionPassport(rawUser.passport))(InvalidPassport)
+    } yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport)
+
 }
