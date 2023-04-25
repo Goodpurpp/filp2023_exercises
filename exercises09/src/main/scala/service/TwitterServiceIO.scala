d3842f5bff9f266b4b80031ae948a8e9ea93f9dd
Goodpurpp <shegolevai14@gmai.com>
2023-04-25 11:12:53 +0500

    process exercises09

--- exercises09/src/main/scala/service/TwitterServiceIO.scala
+++ exercises09/src/main/scala/service/TwitterServiceIO.scala
@@ -2,22 +2,52 @@ package service
 
 import cats.effect.IO
 import cats.syntax.all._
-
+import service.TwitterService
+import service.domain.GetTweetResponse.{Found, NotFound}
 import service.domain._
-
-import twitter.domain.TwitterError._
 import twitter.TwitterApi
+import twitter.domain.TwitterError._
 import twitter.domain._
+
+import scala.util.{Failure, Success, Try}
 
 // Воспользуйтесь синтаксисом map, recover, traverse из cats.syntax.all_
 class TwitterServiceIO(api: TwitterApi) extends TwitterService[IO] {
-  def tweet(user: User, text: String): IO[TweetId] = ???
+  def tweet(user: User, text: String): IO[TweetId] = {
+    io[TweetId](api.tweet(user, text))
+  }
 
-  def like(user: User, tweetId: TweetId): IO[Unit] = ???
+  def like(user: User, tweetId: TweetId): IO[Unit] = {
+    io[Unit](api.like(user, tweetId)).recover {
+      case LikeAlreadyExistError => ()
+    }
+  }
 
-  def unlike(user: User, tweetId: TweetId): IO[Unit] = ???
+  def unlike(user: User, tweetId: TweetId): IO[Unit] = {
+    io(api.unlike(user, tweetId)).recover {
+      case LikeNotExistError => ()
+    }
+  }
 
-  def getTweet(tweetId: TweetId): IO[GetTweetResponse] = ???
+  def getTweet(tweetId: TweetId): IO[GetTweetResponse] = {
+    io[TweetInfo](api.get(tweetId)).redeem(_ => NotFound(tweetId), x => Found(x))
+  }
 
-  def getTweets(ids: List[TweetId]): IO[GetTweetsResponse] = ???
+  def io[T](f: (Try[T] => Unit) => Unit): IO[T] = {
+    IO.async(cb => f(cb.compose(_.toEither)))
+  }
+
+  def getTweets(ids: List[TweetId]): IO[GetTweetsResponse] =
+    ids
+      .traverse(getTweet)
+      .map(x =>
+        x.foldLeft(GetTweetsResponse(Set.empty[TweetId], Set.empty[TweetInfo]))((tweetsResponse, current) => {
+          val notFound = tweetsResponse.notFound
+          val found    = tweetsResponse.found
+          current match {
+            case Found(v)    => GetTweetsResponse(notFound, found + v)
+            case NotFound(v) => GetTweetsResponse(notFound + v, found)
+          }
+        })
+      )
 }
